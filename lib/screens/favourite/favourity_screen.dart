28fd897ffafd984eae55dbd53c479c05ae1e006a
tranry <wwvww9@gmail.com>
2024-01-10 11:34:20 +0700

    tuan2

---
+++ lib/screens/favourite/favourity_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FavourityScreen extends StatefulWidget {
+  const FavourityScreen({super.key});
+
+  @override
+  State<FavourityScreen> createState() => _FavourityScreenState();
+}
+
+class _FavourityScreenState extends State<FavourityScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
